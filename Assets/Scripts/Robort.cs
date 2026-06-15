8fb175c953eff3d9230e2a546d970c5873a5dfd1
Utsav0706 <utsavjivani.07@gmail.com>
2026-06-15 15:31:15 +0100

    Working Robot

---
+++ Assets/Scripts/Robort.cs
@@ -0,0 +1,28 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robort : MonoBehaviour
+{
+    FirstPersonController player;
+    NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = FindFirstObjectByType<FirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(player.transform.position);
+    }
+}
